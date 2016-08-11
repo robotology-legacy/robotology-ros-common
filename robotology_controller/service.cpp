801ebdaafbe02fa9f057fca40b76e0e8e3a9aa1f
icub <alberto.cardellino@iit.it>
2016-08-11 14:33:59 +0200

    Fix compilation error for dummy service

--- robotology_controller/service.cpp
+++ robotology_controller/service.cpp
@@ -4,8 +4,8 @@
 bool add(robotology_msgs::controlBoardSrv::Request  &req,
          robotology_msgs::controlBoardSrv::Response &res)
 {
-    res.success = (req.ciao == 42 ? true : false);
-    ROS_INFO("request: x=%ld -> response %d\n", (long int)req.ciao, (int) res.success);
+    res.success = (req.test == 42 ? true : false);
+    ROS_INFO("request: x=%ld -> response %d\n", (long int)req.test, (int) res.success);
 //     ROS_INFO("sending back response: [%ld]", (long int)res.sum);
     return true;
 }
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "add_two_worlds_server");
     ros::NodeHandle n;
 
-    ros::ServiceServer service = n.advertiseService("ciao", add);
+    ros::ServiceServer service = n.advertiseService("test", add);
     ROS_INFO("Ready to rule the world.");
     ros::spin();
 
